5e9f13495745c7fdaa4756e26b70dadcf4ff0fc3
Johan Gonzalez Gonzalez <121596554+JonusGhost@users.noreply.github.com>
2025-01-18 21:07:46 -0600

    Primer commit

---
+++ routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::post('login', [LoginController::class,'login']);
+
+Route::post('register', [RegisterController::class,'register']);
